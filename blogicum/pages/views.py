01078c0b5a419b0771db0a0443f528787bc16c3f
Timur Khasyanov <hasyanov21052005@gmail.com>
2024-09-13 09:15:35 +0300

    Исправил PEP8

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
+
+def index(request):
+    return render(request, 'pages/index.html')
+
+
 def about(request):
     return render(request, 'pages/about.html')
-
-def rules(request):
-    return render(request, 'pages/rules.html')
