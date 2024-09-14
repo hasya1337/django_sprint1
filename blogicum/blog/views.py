15b14fcf65bce98635c5a300420dac3578502772
Timur Khasyanov <hasyanov21052005@gmail.com>
2024-09-14 08:35:26 +0300

    Исправил PEP8

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -65,5 +65,3 @@ def post_detail(request, id):
 def category_posts(request, category_slug):
     filtered_posts = [post for post in posts if post['category'] == category_slug]
     return render(request, 'blog/category.html', {'posts': filtered_posts, 'category_slug': category_slug})
-
-
